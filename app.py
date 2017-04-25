5cec1174c7ac61df1febc2adba42fad7d0037c74
serratug <tugs@itu.edu.tr>
2017-04-25 14:30:21 +0300

    jkjlk

--- app.py
+++ app.py
@@ -43,11 +43,19 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     
                     words = message_text.split(",")
-                    list = words[1].split()
-                    list2 = words[2].split()
                     
+                    result = flights_service.get_result(
+                                                        country='UK',
+                                                        currency='GBP',
+                                                        locale='en-GB',
+                                                        originplace='SIN-sky',
+                                                        destinationplace='KUL-sky',
+                                                        outbounddate='2017-05-28',
+                                                        inbounddate='2017-05-31',
+                                                        adults=1).parsed
+                    ucus = json.dumps(result)
 
-                    send_message(sender_id, words[1])
+                    send_message(sender_id, ucus)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
