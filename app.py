d81e93825f276d4c15f945f4a64feed2673ae897
serratug <tugs@itu.edu.tr>
2017-05-01 18:27:29 +0300

    s

--- app.py
+++ app.py
@@ -44,9 +44,11 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
                     
-                    words = [4]
                     words = message_text.split(",")
-                    url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/%s/%s/%s/?apikey=prtl6749387986743898559646983194" % (words[1], words[2], words[0])
+                    outbounddate = words[0]
+                    origin = words[1]
+                    destination = words[2]
+                    url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/%s/%s/%s/?apikey=prtl6749387986743898559646983194" % (origin, destination, outbounddate)
                     
                     #url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/IST/ESB/2017-10-25/2017-11-11?apikey=prtl6749387986743898559646983194"
                     log(url)
