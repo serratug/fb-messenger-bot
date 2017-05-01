e493ce1b6f674745e420085a503a1d55b456c683
serratug <tugs@itu.edu.tr>
2017-05-01 23:10:59 +0300

    y

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     echo = messaging_event["message"]["is_echo"]
                     
-                    if echo=="True":
+                    if len(message_text)>20:
                         sys.exit(1)
                     
                     log("message = " + message_text)
@@ -59,7 +59,7 @@ def webhook():
 
 #url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/%s/%s/%s/?apikey=prtl6749387986743898559646983194" % (origin, destination, outbounddate)
                     
-                    url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/IST/ESB/2017-10-25/2017-11-11?apikey=prtl6749387986743898559646983194"
+                    url ="http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/TR/usd/tr-TR/IST/ESB/2017-08-25/2017-09-11?apikey=prtl6749387986743898559646983194"
                     log("print url = " + url)
                     
                     f = requests.get(url)
